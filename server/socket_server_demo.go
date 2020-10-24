570d829068113db62f2662a7e4400475e84a8239
webcrawler <1274219974@qq.com>
2020-10-24 17:01:50 +0800

    go socket demo

--- server/socket_server_demo.go
+++ server/socket_server_demo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"log"
 	"net"
@@ -18,17 +19,48 @@ func handleConn(c net.Conn) {
 			return
 		}
 		log.Printf("read %d bytes, content is:%s\n", n, string(buf[:n]))
-		log.Printf("val:%d", binary.BigEndian.Uint32(buf[:n])) // LittleEndian  BigEndian
+		log.Printf("val:%d", binary.LittleEndian.Uint32(buf[:n])) // LittleEndian  BigEndian
 
 		// write to the connection
 		log.Println("start to write")
-		words := "1234567890"
-		c.Write([]byte(words))
+		//words := "22"
+		//c.Write([]byte(words))
+
+
+		// add by joke
+		type A struct {
+			One int16
+			Two int32
+			Three int8
+			Four float32
+			Five float64
+		}
+		var a A
+		a.One = int16(2 + 4 + 1 + 4 + 8) // 2 + 4 + 5
+		a.Two = int32(123654)
+		a.Three = int8(8)
+		a.Four = float32(123.123)
+		a.Five = float64(123654789)
+		var bufxx = new(bytes.Buffer)
+		binary.Write(bufxx,binary.LittleEndian,a) // LittleEndian
+		c.Write(bufxx.Bytes())
+
+		//type Packet struct {
+		//	size int32
+		//	data int32
+		//}
+		//var buffer bytes.Buffer
+		//var packet = new(Packet)
+		//packet.size = 8
+		//packet.data = 123654
+		//binary.Write(&buffer, binary.BigEndian, &packet)
+		//c.Write(buffer.Bytes())
 	}
 }
 
 func main() {
-	l, err := net.Listen("tcp", "127.0.0.1:8888")
+	// l, err := net.Listen("tcp", "127.0.0.1:8888")
+	l, err := net.Listen("tcp", "10.10.10.3:8077")
 	if err != nil {
 		log.Println("error listen:", err)
 		return
